d6eb7554be98b815d59fd6736008a48f01480b28
ItsRahim <rahim1605@gmail.com>
2022-08-15 12:07:12 +0100

    updated form file html

--- src/main/java/com/rahim/eccomerce/resource/ItemResource.java
+++ src/main/java/com/rahim/eccomerce/resource/ItemResource.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
-//TODO change to @Controller
 @Controller
 @RequestMapping("/item")
 @RequiredArgsConstructor
@@ -39,10 +36,10 @@ public class ItemResource {
         return "get-items";
     }
 
-    @PostMapping("/save")
-    public String saveItem(@RequestBody @Valid Item item) {
-        //itemService.create(item)
-        return "Save";
+    @GetMapping("/save")
+    public String saveItem(Model model) {
+        model.addAttribute("item", new Item());
+        return "add-item";
     }
 
     @DeleteMapping("/delete/{id}")
