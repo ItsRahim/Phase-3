8d9640e56a485454ed561f31920e8a4a47b2f1df
ItsRahim <rahim1605@gmail.com>
2022-08-16 13:47:50 +0100

    changed spacing

--- src/main/java/com/rahim/eccomerce/repository/CustomerRepo.java
+++ src/main/java/com/rahim/eccomerce/repository/CustomerRepo.java
@@ -3,5 +3,5 @@ package com.rahim.eccomerce.repository;
 import com.rahim.eccomerce.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepo extends JpaRepository<Customer, Long>{
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 }
